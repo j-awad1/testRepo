4279b54c1a950b2f14821121c270772ac36e1722
jonathana <jonathana@vt.edu>
2024-05-16 20:16:17 -0400

    adding python calc

---
+++ main.py
@@ -0,0 +1,5 @@
+from operations import CalcOperations
+
+
+if __name__=="__main__":
+    print(CalcOperations.mult(3,2))
---
+++ operations.py
@@ -0,0 +1,22 @@
+# write operations class for a calculator 4 function, call operations class inside of main method (in a different file), add to git
+class CalcOperations:
+
+    def getN1(self):
+        return self.n1
+
+    def add(self):
+        ans = self.n1 + self.n2
+        return ans
+
+    def sub(self, n1, n2):
+        ans = n1 - n2
+        return ans
+
+    def mult(n1,n2):
+        ans = n1 * n2
+        return ans
+
+    def div(n1,n2):
+        ans = n1/n2
+        return ans
+
